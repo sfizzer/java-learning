e70aeeddf25401d25217cae3818b18bd720ae97a
juno <jjzee2023@gmail.com>
2024-08-11 18:34:27 -0500

    update 1.2
    Update to 1.1
    -fixed launch.json issue
    -fixed .java extension

---
+++ One.java
@@ -0,0 +1,11 @@
+/*
+ * This is a simple java program.
+ * Call this file One.java
+ */
+
+public class One {
+    // Your program begins with a call to main()
+    public static void main(String arg[]) {
+        System.out.println("This is a simple Java program");
+    }
+}
