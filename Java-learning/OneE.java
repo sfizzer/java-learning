d17c2d4455ef7d75a66ac753ae89326d8289e6a4
juno <jjzee2023@gmail.com>
2024-08-12 12:35:50 -0500

    update 1.7
    Update to 1.E
    -Content added

---
+++ Java-learning/OneE.java
@@ -0,0 +1,21 @@
+/*
+ * Demonstrate a block of code.
+ * 
+ * Call this file "OneE.java"
+ */
+
+public class OneE {
+    public static void main(String args[]) { 
+        int x, y;
+
+        y = 20;
+        // Block target
+        
+        for (x = 0; x<10; x++) { 
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+        y = y - 2;
+        
+        }
+    }
+}
